9c67599c1118820c179993027e9c913b8a4eafd5
Cameron Greenlee <greenlee.cameron319@gmail.com>
2022-12-13 09:23:47 -0500

    email authentication working

--- InternalInterviewService/models.py
+++ InternalInterviewService/models.py
@@ -1,14 +1,60 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
-class User(AbstractUser):
-    # name
-    # email
-    # codename
+
+class UserManager(BaseUserManager):
+  def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+    if not email:
+        raise ValueError('Users must have an email address')
+    if not password:
+        raise ValueError('Please provide a password')
+    now = timezone.now()
+    email = self.normalize_email(email)
+    user = self.model(
+        email=email,
+        is_staff=is_staff, 
+        is_active=True,
+        is_superuser=is_superuser, 
+        last_login=now,
+        date_joined=now, 
+        **extra_fields
+    )
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+  def create_user(self, email, password, **extra_fields):
+    return self._create_user(email, password, False, False, **extra_fields)
+
+  def create_superuser(self, email, password, **extra_fields):
+    user=self._create_user(email, password, True, True, **extra_fields)
+    return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=254)
+    last_name = models.CharField(max_length=254)
+    codename = models.CharField(max_length=20)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'codename']
+
+    objects = UserManager()
+
+    def get_absolute_url(self):
+        return "/users/%i/" % (self.pk)
+
     # personal_pitch
     # links
-    pass
-    def __str__(self):
-        return self.email
+
 
