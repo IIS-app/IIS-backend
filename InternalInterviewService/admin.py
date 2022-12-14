b5c4130c6f835798b9780e152c1063147cdc7b8d
Cameron Greenlee <greenlee.cameron319@gmail.com>
2022-12-14 17:19:32 -0500

    added Question model to admin

--- InternalInterviewService/admin.py
+++ InternalInterviewService/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Win
+from .models import User, Win, Question
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Win)
+admin.site.register(Question)
