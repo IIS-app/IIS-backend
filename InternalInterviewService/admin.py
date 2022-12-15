4cfac09b5b7ffe3e32da492401fdce6d63b8125f
Cameron Greenlee <greenlee.cameron319@gmail.com>
2022-12-15 12:00:50 -0500

    added models to admin

--- InternalInterviewService/admin.py
+++ InternalInterviewService/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import User, Win, Question
+from .models import User, Win, Question, TargetCompany, CompanyContacts, StarrQuestions, CoverLetter, Resume
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Win)
-admin.site.register(Question)
+admin.site.register(TargetCompany)
+admin.site.register(CompanyContacts)
+admin.site.register(StarrQuestions)
+admin.site.register(CoverLetter)
+admin.site.register(Resume)
