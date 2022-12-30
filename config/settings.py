909aa43bee1793cf6e0f63cca62cee796d3dfda6
Cameron Greenlee <greenlee.cameron319@gmail.com>
2022-12-29 19:54:16 -0500

    media typo

--- config/settings.py
+++ config/settings.py
@@ -159,5 +159,5 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-MEADIA_URL = '/media/'
-MEADIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
