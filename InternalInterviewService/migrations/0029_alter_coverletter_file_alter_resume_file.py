4993d39938c8e47c3df52f4b07c37d3dcadaca32
Cameron Greenlee <greenlee.cameron319@gmail.com>
2023-01-07 11:02:35 -0500

    fixed migration error

--- InternalInterviewService/migrations/0029_alter_coverletter_file_alter_resume_file.py
+++ InternalInterviewService/migrations/0029_alter_coverletter_file_alter_resume_file.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('InternalInterviewService', '0028_shortpersonalpitch_longpersonalpitch_links'),
+        ('InternalInterviewService', '0027_alter_win_win_picture'),
     ]
 
     operations = [
