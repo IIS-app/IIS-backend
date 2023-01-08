b211b40cbbdeef9ba3abb52627cc4fcd67ee322d
Cameron Greenlee <greenlee.cameron319@gmail.com>
2023-01-08 16:19:03 -0500

    add system questions to admin

--- InternalInterviewService/admin.py
+++ InternalInterviewService/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Win, Question, TargetCompany, CompanyContacts, StarrQuestions, CoverLetter, Resume, Interview, Job, Dossier, Goal
+from .models import User, Win, Question, SystemQuestion, TargetCompany, CompanyContacts, StarrQuestions, CoverLetter, Resume, Interview, Job, Dossier, Goal
 
 # Register your models here.
 
@@ -15,3 +15,4 @@ admin.site.register(Interview)
 admin.site.register(Job)
 admin.site.register(Dossier)
 admin.site.register(Goal)
+admin.site.register(SystemQuestion)
