54aa431b1f7895b1923b29a2633ffd820925484a
Cameron Greenlee <greenlee.cameron319@gmail.com>
2023-01-08 16:30:35 -0500

    remove user fk from system questions

--- InternalInterviewService/models.py
+++ InternalInterviewService/models.py
@@ -98,7 +98,6 @@ class SystemQuestion(models.Model):
   ]
   question_type = models.CharField(max_length=2,choices=QUESTION_TYPE)
   question = models.CharField(max_length=75)
-  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
   
   tags = TaggableManager(blank=True)
 
