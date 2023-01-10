c120c940ccf13adba5ccb741bd2cfb15fbf8cec0
Cameron Greenlee <greenlee.cameron319@gmail.com>
2023-01-10 13:33:58 -0500

    fixing bucket

--- config/settings.py
+++ config/settings.py
@@ -167,7 +167,7 @@ if env('USE_S3'):
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-    # AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_S3_FILE_OVERWRITE = False
